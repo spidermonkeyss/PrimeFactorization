d0e88d57f1b532754317f05d4b3c178090fa286b
ahill14 <andrewhill@columbus.rr.com>
2020-07-05 06:40:50 -0400

    Inital Commit

---
+++ PrimeFactorization/PrimeFactorization/Program.cs
@@ -0,0 +1,59 @@
+﻿//Prime Factorization – Have the user enter a number and find all Prime Factors (if there are any) and display them.
+using System;
+using System.Collections.Generic;
+using Utlity;
+
+namespace PrimeFactorization
+{
+    class Program
+    {
+        static List<int> primeNumbers = new List<int>();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to check for prime factors");
+            int input = UserInput.GetUserNumericInput();
+            CheckIfPrimeNumber(input);
+            string output = "Prime factors: ";
+            foreach (int num in primeNumbers)
+            {
+                output += num + ", ";
+            }
+            output = output.Substring(0, output.Length - 2);
+            Console.WriteLine(output);
+        }
+
+        static bool CheckIfPrimeNumber(int baseNumber)
+        {
+            List<int> wholeNumbers = new List<int>();
+
+            float halfValue = (float)baseNumber / 2.0f;
+            for (int i = 2; i < halfValue; i++)
+            {
+                int remainder = baseNumber % i;
+                float num = (float)baseNumber / (float)i;
+                //Console.Write(baseNumber + " / " + i);
+                if (remainder == 0)
+                {
+                    //Console.WriteLine("     " + num + ":whole");
+                    wholeNumbers.Add(i);
+                    wholeNumbers.Add((int)num);
+                    if (CheckIfPrimeNumber(i))
+                        primeNumbers.Add(i);
+                    if (CheckIfPrimeNumber((int)num))
+                        primeNumbers.Add((int)num);
+                    break;
+                }
+                else
+                {
+                    //Console.WriteLine("     " + num +":not");
+                }
+            }
+
+            if (wholeNumbers.Count == 0)
+                return true;
+
+            return false;
+        }
+    }
+}
---
+++ PrimeFactorization/PrimeFactorization/UserInput.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Utlity
+{
+    public class UserInput
+    {
+        public static int GetUserNumericInput()
+        {
+            int intInput = 0;
+
+            string input = Console.ReadLine();
+            bool isNumber = int.TryParse(input, out int n);
+            if (!isNumber)
+            {
+                Console.WriteLine("Not a number");
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+            }
+            intInput = Convert.ToInt32(input);
+            return intInput;
+        }
+    }
+}
